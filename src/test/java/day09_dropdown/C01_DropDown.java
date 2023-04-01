3dd218cc1e0befd0cbfc570fcdf3a59fde675e13
Tolga Dumanlar <dumanlartolga@gmail.com>
2023-04-01 22:15:22 +0300

    V1

---
+++ src/test/java/day09_dropdown/C01_DropDown.java
@@ -0,0 +1,14 @@
+package day09_dropdown;
+
+public class C01_DropDown {
+    /*
+    -DropDown Nedir:Altbaşlıkların olduğu açılır menü listesi.
+    -DropDown nasıl automate edilir (Handle):
+            a)DropDown menuyu ilk olarak locate ederiz.
+            b)Select objesi oluştururuz.
+            c)Select objesinin dropdown menu webelementinin içeriğine ve seçeneklerine
+            erişim sağlaması için SELECT sınıfına argüman olarak locate ettiğimiz webelementi koyarız.
+            Syntax:Select select=new Select(dropdown menu webelement)
+            d)Select class'ı, sadece <select> tag'ı ile oluşturulmuş dropdown menulere uygulanabilir.
+     */
+}

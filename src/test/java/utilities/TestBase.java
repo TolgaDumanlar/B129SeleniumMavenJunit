7352cf47faf385633bceac628e95da4174aeddc6
Tolga Dumanlar <dumanlartolga@gmail.com>
2023-04-03 13:05:22 +0300

    V1

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -3,9 +3,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
 public abstract class TestBase {
@@ -21,7 +23,54 @@ public abstract class TestBase {
     }
     @After
     public void tearDown() throws Exception {
-        Thread.sleep(3000);
-        driver.quit();
+        //Thread.sleep(3000);
+        //driver.quit();
     }
+    //HARD WAIT
+    public static void bekle(int saniye){
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Alert ACCEPT
+    public static void alertAccept(){
+        driver.switchTo().alert().accept();
+    }
+
+    //Alert DISMISS
+    public static void alertDismiss(){
+        driver.switchTo().alert().accept();
+    }
+
+    //Alert getText()
+    public static void alertText(){
+        driver.switchTo().alert().accept();
+    }
+
+    //Alert promptBox()
+    public static void alertPrompt(String text){
+        driver.switchTo().alert().sendKeys(text);
+    }
+
+    //DropDown VisibleText
+    /*
+        Select select2 = new Select(gun);
+        select2.selectByVisibleText("7");
+
+        //ddmVisibleText(gun,"7"); --> Yukarıdaki kullanım yerine sadece method ile handle edebilirim
+     */
+    public static void ddmVisibleText(WebElement ddm,String secenek){
+        Select select = new Select(ddm);
+        select.selectByVisibleText(secenek);
+        }
+
+    //DropDown Value
+    public static void ddmValue(WebElement ddm,String secenek){
+        Select select = new Select(ddm);
+        select.selectByValue(secenek);
+    }
+
 }
